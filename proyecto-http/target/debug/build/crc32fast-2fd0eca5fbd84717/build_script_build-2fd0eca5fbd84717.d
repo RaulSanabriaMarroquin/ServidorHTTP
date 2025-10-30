087afa75a49ffd501aa5cd15caf70ee8784e4792
David Acuña López <89756491+davidacunalopez@users.noreply.github.com>
2025-10-29 19:07:24 -0600

    metricas

--- proyecto-http/target/debug/build/crc32fast-2fd0eca5fbd84717/build_script_build-2fd0eca5fbd84717.d
+++ proyecto-http/target/debug/build/crc32fast-2fd0eca5fbd84717/build_script_build-2fd0eca5fbd84717.d
@@ -1,5 +1,5 @@
-C:\Users\Raul\Desktop\TEC\SO\ServidorHTTP\proyecto-http\target\debug\build\crc32fast-2fd0eca5fbd84717\build_script_build-2fd0eca5fbd84717.d: C:\Users\Raul\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
+C:\Users\USUARIO\Documents\Github\ServidorHTTP\proyecto-http\target\debug\build\crc32fast-2fd0eca5fbd84717\build_script_build-2fd0eca5fbd84717.d: C:\Users\USUARIO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
 
-C:\Users\Raul\Desktop\TEC\SO\ServidorHTTP\proyecto-http\target\debug\build\crc32fast-2fd0eca5fbd84717\build_script_build-2fd0eca5fbd84717.exe: C:\Users\Raul\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
+C:\Users\USUARIO\Documents\Github\ServidorHTTP\proyecto-http\target\debug\build\crc32fast-2fd0eca5fbd84717\build_script_build-2fd0eca5fbd84717.exe: C:\Users\USUARIO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs
 
-C:\Users\Raul\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs:
+C:\Users\USUARIO\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crc32fast-1.5.0\build.rs:
